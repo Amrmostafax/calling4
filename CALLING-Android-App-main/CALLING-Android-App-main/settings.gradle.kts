e042d978e595127d0d345b9730119d44b2207743
Amrmostafax <160327583+Amrmostafax@users.noreply.github.com>
2025-11-11 19:42:19 +0200

    Delete CALLING-Android-App-main/CALLING-Android-App-main directory

--- CALLING-Android-App-main/CALLING-Android-App-main/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-// This file tells Gradle what modules to include in the build.
-// In our case, it's just the "app" module.
-
-rootProject.name = "CALLING" // This is your project's name
-include(":app")
