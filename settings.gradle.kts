3b4c7ad9a84ffb58e8638b75954d8b2abe055fde
Amrmostafax <160327583+Amrmostafax@users.noreply.github.com>
2025-11-14 00:55:44 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,21 @@
 // This file tells Gradle what modules to include in the build.
 // In our case, it's just the "app" module.
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "calling4"
 include(":app")
